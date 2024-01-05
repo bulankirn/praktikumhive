d529431b52492a85c8c0c3737bde8d1345975d59
bulankirn <bulankirana.hobie@gmail.com>
2024-01-05 15:55:09 +0700

    first init github

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:praktikumhive/model/person.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'screens/info_screens.dart';
+
+
+main() async {
+// Initialize hive
+  await Hive.initFlutter();
+// Registering the adapter
+  Hive.registerAdapter(PersonAdapter());
+// Opening the box
+  await Hive.openBox('peopleBox');
+  runApp(MyApp());
+}
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+class _MyAppState extends State<MyApp> {
+  @override
+  void dispose() {
+// Closes all Hive boxes
+    Hive.close();
+    super.dispose();
+  }
+
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        title: 'Hive Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: InfoScreen(),
+      );
+    }
+  }
